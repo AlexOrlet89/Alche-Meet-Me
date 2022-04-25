5cafdb02a27483d7d73fe6ca8777faf7f29ff811
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-25 15:50:51 -0700

    app.test.js gets header image alt text and name from header

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,22 @@
+import { screen } from '@testing-library/react'
+import Header from './components/layout/Header'
 
-test('Should render the header', () => {
+const { render } = require('@testing-library/react')
 
+const user = {
+  id: 1,
+  created_at: '2021-12-13T00:17:29+00:00',
+  name: 'Vonta',
+  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+  motto: 'Res Non Verba',
+  color: 'crimson',
+}
+
+test('Should render the header', async () => {
+  render(<Header user={user} />)
+  screen.debug()
+  screen.getByAltText('Alchemy Logo')
+  screen.findByText('Meet Vonta!')
 })
