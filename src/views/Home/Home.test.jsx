96196c0104f81d298f877dc9e43b9e47a17d34d7
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-25 16:37:09 -0700

    Profile.test.js tests work too

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,3 +1,6 @@
+import { screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import Profile from '../../components/Profile/Profile'
 
 const user = {
   id: 1,
@@ -11,5 +14,11 @@ const user = {
 }
 
 test('Should render the user profile', () => {
-
+  render(<Profile user={user} />)
+  screen.findByText('Vonta')
+  screen.findByText('Res Non Verba')
+  screen.findByText('Interests')
+  screen.findByText('React')
+  screen.getByAltText('avatar')
+  screen.getByAltText('header')
 })
